0cd68abcdb884664d48673b5f40ae00194a5d2e6
aoisupersix <aoisupersix@gmail.com>
2019-01-11 23:35:25 +0900

    [update] デザインを調整

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
 <div id="app">
   <h1 class="display-1" style="text-align: center;">Hayakawa Laboratory</h1>
-  <b-card-group columns class="md-1">
-    <b-card bg-variant="primary"
-      header="早川"
+  <div class="container-fluid">
+    <b-card no-body
       class="text-center status-card card-primary"
-      v-for="member in members">
-      <p class="card-text text-center">在室</p>
+      v-for="member in members"
+      :key="member"
+      :class="'card-' + states[member.status].color">
+      <b-link to="/" class="btn btn-fix">
+        <h4 class="card-header">{{ member.last_name + " " + member.first_name }}</h4>
+        <b-card-body>
+          <p class="card-text">{{ states[member.status].name }}</p>
+        </b-card-body>
+      </b-link>
     </b-card>
-    </b-card-group>
+  </div>
 </div>
 </template>
 
@@ -23,22 +29,46 @@ import CardList from './components/CardList.vue';
   },
 })
 export default class App extends Vue {
-  private members = [1,2,3];
+  private states = [];
+  private members = [];
 
   public async created(): Promise<void> {
     const stateSnap = await firebaseDatabase.ref('status').once('value');
-    console.log(stateSnap.val());
+    this.states = stateSnap.val();
+    firebaseDatabase.ref('members').on('value', (snap) => {
+      this.members = snap!.val();
+    });
   }
 }
 </script>
 <style>
  .status-card {
+   display: inline-block !important;
    margin: 5px;
-   max-width: 11.2rem;
-   max-height: 9.3rem;
+   width: 11.2rem;
+   height: 9.3rem;
  }
  .card-primary {
    background-color: #65ace4 !important;
    color: #ffffff !important;
  }
+ .card-warning {
+   background-color: #de9610 !important;
+   color: #ffffff !important;
+ }
+ .card-secondary {
+   background-color: #fbfbfb !important;
+   color: #040404 !important;
+ }
+ .card-text {
+   font-size: 2.5rem;
+ }
+ .btn-fix {
+  width: 11.2rem;
+  height: 9.3rem;
+  padding: 0 !important;
+  border: none !important;
+  white-space: normal;
+  color: inherit !important;
+  }
 </style>
